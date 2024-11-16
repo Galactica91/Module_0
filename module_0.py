fc31f601d40d068365ea42a9077ca5a97333a60e
Galactica91 <osheva91@yandex.com>
2024-11-16 11:06:49 +0300

    Add files via upload

---
+++ module_0.py
@@ -0,0 +1,38 @@
+"1st program"
+from os import remove
+
+numb_1 = 9**0.5*5
+print(numb_1)
+
+"2nd program"
+
+numb_2 = 9.99 > 9.98 and 1000 != 1000.1
+print(numb_2)
+
+"3rd program"
+
+numb_3 = 2*2+2
+numb_4 = 2*(2+2)
+differ = numb_3 == numb_4
+
+print(numb_3, numb_4)
+print(differ)
+
+"4th program"
+
+str1 = '123.456'
+print(str1[4])
+str1 = float(123.456 * 10)
+print(str1)
+
+"5th program"
+
+str2 = '88.88'
+str3 = '888.9'
+
+str2 = float(88.88 * 10)
+str3 = float(888.9)
+print(str2, str3)
+differ2 = str2 > str3
+print(differ2)
+
